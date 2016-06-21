5147a9c0b68e398b49018149e8c38ff1aff2d015
shibin <shibin@mac>
2016-06-22 00:21:51 +0800

    core 中 准备用 pty 写了执行原始终端命令的代理类，

---
+++ hdfshell/core.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+#author@shibin
+#2016.06.21
+
+import pty
+
+
+class commandProxy:
+    """ 代理 命令"""
+    def __init__(self):
+        """ 一个执行原始terminal命令的session"""
+        self.session = None
+
+    def runChild(cmd):
+        pass
