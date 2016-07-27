6d45ddc63048218c05df9f7242fc2f15257a2580
alingse <alingse@foxmail.com>
2016-07-28 01:48:01 +0800

    update 更改了Explain 和Proxy两个类，增加了一点函数

--- hdfshell/explain.py
+++ hdfshell/explain.py
@@ -2,42 +2,46 @@
 #author@alingse
 #2016.07.20
 
-from cluster import hdfs_schema,file_schema
+from cluster import hdfs_schema
+from cluster import file_schema
+
+import argparse
+
 
 class commandExplain(object):
+
+    def __init__(self,name,praser):
+        self.name = name
+        self.parser = praser
+
+    def isValid(self,*args,**kwargs):
+        pass
+
+    def translate(self,*args,**kwargs):
+        pass
+
+
+
+
+class commandProxy(object):
     """ 代理 命令 shell 命令 解释器"""
     def __init__(self,hdfs):
         self.hdfs = hdfs
-        self.in_explain = False
+        self.explains = {}
         self._head = 'hadoop fs '
+        self.lock = None
 
-    def start(self):
-        self.in_explain = True
+    def add_explain(self,explain):
+        name = explain.name
+        if name in self.explains:
+            pass
+        self.explains[name] = explain
 
-    def ls(self,*paths):
-        #do check && clean
-
-
-        uri = self.hdfs.uri
-        uri_head = self.hdfs.uri_head
-
-        _paths = []
-        for path in paths:
-            if path.startswith(file_schema):
-                _paths.append(path)
-            #other hdfs
-            elif path.startswith(hdfs_schema):
-                _paths.append(path)
-            #from /
-            elif path.startswith('/'):
-                _path.append(uri_head+path)
-            #from relative
-            else:
-                _path.append(uri+path)
-
-        command = self._head + ' -ls ' + ' '.join(_path)
-
-        return command
-
-    def explain(self,argv):
+    def remove_explain(self,name):
         pass
+
+    def proxy(self,*args):
+        pass
+        #for exp in self.explains
+
+    def 
