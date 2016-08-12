ee8c25c8b7d7b965c308f2cd92e9c67af63bb141
alingse <alingse@foxmail.com>
2016-08-12 16:26:17 +0800

    add:添加explain的API

--- hdfshell/explain.py
+++ hdfshell/explain.py
@@ -7,13 +7,15 @@ import argparse
 
 class commandExplain(object):
     """docstring for command"""
-    def __init__(self, name, parser):
+    def __init__(self, name, parser, lineable=False):
         self.name = name
         self.parser = parser
+        self.lineable = lineable
 
-    def isVaild(self,args):        
-        args = self.parser.parse_args(args)
-        return args
+    def isVaild(self,args):
+        pass
+        #args = self.parser.parse_args(args)
+        #return args
 
     
     
@@ -21,10 +23,10 @@ class LS(commandExplain):
     """ docstring for ls """
     def __init__(self):
         name = 'ls'
-        ls_parser = argparse.ArgumentParser()
-        ls_parser.add_argument('path', nargs='+', help='hdfs path')
+        parser = argparse.ArgumentParser()
+        parser.add_argument('path', nargs='+', help='hdfs path')
         
-        super(LS, self).__init__(name,ls_parser)
+        super(LS, self).__init__(name,parser,lineable=True)
 
     def translate(self,args,env):
         cluster = env['cluster']
@@ -36,20 +38,58 @@ class LS(commandExplain):
         cmd = 'hadoop fs -{} '.format(self.name)+' '.join(paths)
         return cmd
 
+    def tranline(self,line):
+        pass
+
+    def tranout(self,out):
+        pass
+
+    def tranerr(self,err):
+        pass
 
 
-class EXIT(commandExplain):
-    """ docstring for exit """
+class Enter(commandExplain):
+    """docstring for Enter"""
     def __init__(self):
-        name = 'exit'
-        exit_parser = argparse.ArgumentParser()        
-        super(EXIT, self).__init__(name,exit_parser)
+        name = ''
+        parser = argparse.ArgumentParser()
+        super(Enter, self).__init__(name,parser)
 
     def translate(self,args,env):
         pass
 
+    def tranline(self,line):
+        pass
+
+    def tranout(self,out):
+        pass
+
+    def tranerr(self,err):
+        pass
+
+        
+class EXIT(commandExplain):
+    """ docstring for exit """
+    def __init__(self):
+        name = 'exit'
+        parser = argparse.ArgumentParser()        
+        super(EXIT, self).__init__(name,parser)
+
+    def translate(self,args,env):
+        pass
+
+    def tranline(self,line):
+        pass
+
+    def tranout(self,out):
+        pass
+
+    def tranerr(self,err):
+        pass
 
 
 ls = LS()
 exit = EXIT()
+enter = Enter()
+
 
