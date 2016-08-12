b4f0e3c211ae5cca7db5977a47b3e7dc099fc2b9
alingse <alingse@foxmail.com>
2016-08-12 16:32:32 +0800

    add:相应修改

--- hdfshell/main.py
+++ hdfshell/main.py
@@ -6,31 +6,45 @@ from __future__ import print_function
 
 from cluster import hdfsCluster
 from execute import initRunEnv
-from execute import readline
-from proxy import proxyer
+from execute import readline,readerr,readall
 
-env = {}
+from explain import explainList
+from proxy import commandProxy
+
+
 
 def hdfsh():
 
     runbox = initRunEnv()
+    
     env = {}
-    env['cluster'] = hdfsCluster()
+    env['cluster'] = hdfsCluster('hadoop70')
+    
+    proxyer = commandProxy(explainList,env)
 
     while True:
-
         command = raw_input('box-sh$')
+        
         cmd = proxyer.translate(command,env)
+
         if cmd == None:
             continue
+
         if cmd == False:
             print('this command not in ',command)
             continue
 
         child = runbox(cmd)
-        for line in readline(child):
-            print(line,end = '')
-        print(child.poll())
+        if proxyer.explainer.lineable :
+            for line in readline(child):
+                print(line,end = '')
+            if child.poll() != 0:
+                err = readerr(child)
+                sys.stderr.write(err)
+        else:
+            out,err = readall(child)
+            sys.stdout.write(out)
+            sys.stderr.write(err)
 
 if __name__ == '__main__':
     hdfsh()
